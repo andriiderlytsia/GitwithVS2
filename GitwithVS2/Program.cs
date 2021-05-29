339326073d1a0ec2a6d5f3bf16f2fb3994211451
Andrii <andriiderlytsia@gmail.com>
2021-05-29 11:47:40 +0300

    Changed program.cs

--- GitwithVS2/Program.cs
+++ GitwithVS2/Program.cs
@@ -6,7 +6,7 @@ namespace GitwithVS2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Helle git  ");
         }
     }
 }
