53b03e59825dc64d225c71e7f7de0a0d77505455
Andrii <andriiderlytsia@gmail.com>
2021-05-29 11:39:34 +0300

    Add project files.

---
+++ GitwithVS2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitwithVS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
